750bdaef265267a8074189f0239dfb0483d097c0
Joel Tharakan <joeltharakan@Joels-MacBook-Pro.local>
2025-06-25 12:54:33 +0400

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio-Website/', // 👈 change this to your actual GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
