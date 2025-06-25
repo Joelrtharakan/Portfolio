9e3b3c75fdb2d417307169e61c895b05f9102b7c
Joel Tharakan <joeltharakan@Joels-MacBook-Pro.local>
2025-06-25 13:08:18 +0400

    Initial Commit 

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio-Website/', // 👈 change this to your actual GitHub repo name
   plugins: [react()],
+  base: '/Portfolio/', // 👈 this is the fix
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
