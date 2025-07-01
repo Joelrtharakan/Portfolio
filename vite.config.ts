79b59f263e57c5d4dd15c2bc0919ed1c78f94edf
Joel Tharakan <joeltharakan@Joels-MacBook-Pro.local>
2025-07-01 14:17:57 +0400

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // 👈 This is essential for GitHub Pages
+  base: '/portfolio/', // 👈 This is essential for GitHub Pages
 });
